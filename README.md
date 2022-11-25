6e3d932ff57fb12913115a89d1fb86d174dd13fe
silveflare <108029070+silveflare@users.noreply.github.com>
2022-11-26 02:51:52 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# superbench
+wget -qO- https://github.com/silveflare/superbench/superbench.sh | bash
+
+curl -LsO https://github.com/silveflare/superbench/superbench.sh; chmod +x superbench.sh
---
+++ start.sh
@@ -0,0 +1,3 @@
+# superbench.sh
+wget -qO- https://github.com/silveflare/superbench/superbench.sh | bash
+curl -LsO https://github.com/silveflare/superbench/superbench.sh; chmod +x superbench.sh
